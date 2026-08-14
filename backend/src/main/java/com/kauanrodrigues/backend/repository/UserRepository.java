9e1bda276836896d6e26bbb4cce314bd45b10047
Kauan-Ts16 <kauanfranca16@gmail.com>
2026-08-14 09:43:21 -0300

    feat: adiciona persistência e regras de negócio de usuário

---
+++ backend/src/main/java/com/kauanrodrigues/backend/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.kauanrodrigues.backend.repository;
+
+import com.kauanrodrigues.backend.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    Optional<UserModel> findByIdAndActive(UUID id, boolean active);
+
+    List<UserModel> findAllByActive(boolean active);
+
+}
