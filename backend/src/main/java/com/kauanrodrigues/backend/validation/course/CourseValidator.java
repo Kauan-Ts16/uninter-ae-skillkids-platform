d6f4cd35204555e773585a3076495138deadeee0
Kauan-Ts16 <kauanfranca16@gmail.com>
2026-08-16 15:56:14 -0300

    feat: adiciona validações e mapeamento de cursos

---
+++ backend/src/main/java/com/kauanrodrigues/backend/validation/course/CourseValidator.java
@@ -0,0 +1,51 @@
+package com.kauanrodrigues.backend.validation.course;
+
+import com.kauanrodrigues.backend.dto.course.CoursePatchDto;
+import com.kauanrodrigues.backend.dto.course.CoursePostDto;
+import com.kauanrodrigues.backend.exception.ExceptionGeneric;
+import com.kauanrodrigues.backend.model.CourseModel;
+import com.kauanrodrigues.backend.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CourseValidator {
+
+    private final CourseFormatValidator formatValidator;
+
+    private final CourseRepository repository;
+
+
+    public void validateForCreate(CoursePostDto dto) {
+        formatValidator.validateTitleForCreate(dto.title());
+        formatValidator.validateDescriptionForCreate(dto.description());
+
+        validateUniqueTitle(dto.title());
+    }
+
+    public void validateForUpdate(CoursePatchDto dto, CourseModel course) {
+        validateUpdateFields(dto);
+
+        formatValidator.validateTitleForUpdate(dto.title());
+        formatValidator.validateDescriptionForUpdate(dto.description());
+
+        if (dto.title() != null && !course.getTitle().equalsIgnoreCase(dto.title())) {
+            validateUniqueTitle(dto.title());
+        }
+    }
+
+    private void validateUpdateFields(CoursePatchDto dto) {
+        if (dto.title() == null && dto.description() == null) {
+            throw new ExceptionGeneric("No fields provided!", "Provide at least one field to update.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private void validateUniqueTitle(String title) {
+        if (repository.existsByTitleIgnoreCase(title)) {
+            throw new ExceptionGeneric("Title already exists!", "A course with this title already exists.", HttpStatus.CONFLICT);
+        }
+    }
+
+}
